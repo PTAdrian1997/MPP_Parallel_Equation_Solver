96465e99d9d06be1de1a5bb0d8241e650206d112
PTAdrian1997 <poptifreaadrian@gmail.com>
2021-03-18 18:57:28 +0200

    new modification

--- linear_systems.cpp
+++ linear_systems.cpp
@@ -4,6 +4,7 @@
 #include "pure_sequential_system_solver.h"
 #include "system_generator.h"
 #include "parallel_equation_solver.h"
+#include <chrono>
 
 #include <cmath>
 
@@ -13,19 +14,7 @@ char * coefficient_filename = "a_input.txt";
 char * free_temrs_filename = "b_input.txt";
 char * debug_filename = "debug.txt";
 
-bool check_result(double * solution, linear_system_of_equations lse){
-    double err = 0.001;
-    bool result = true;
-    for(int eq_id = 0; eq_id < lse.unknowns_no && result; eq_id++){
-        double s = 0;
-        for(int col_id = eq_id; col_id < lse.unknowns_no; col_id++) s += solution[col_id] * lse.coefficients[eq_id][col_id];
-        cout << "s = " << s << ", lse.free_terms[" << eq_id << "] = " << lse.free_terms[eq_id] << "\n";
-        if(abs(s - lse.free_terms[eq_id]) > err){
-            result = false;
-        }
-    }
-    return result;
-}
+
 
 void generate_system_main(){
     cout << "generate the linear system: \n";
@@ -39,14 +28,22 @@ void solve_system(){
     linear_system_of_equations lse = read_linear_system(coefficient_filename, free_temrs_filename, MAX_SIZE_N);
     cout << "solve the system: \n";
     int number_of_threads = 4;
-    double * solution = parallel_system_solver(lse, number_of_threads);
-    cout << "check solution: \n";
-    cout << "solution is correct: " << check_result(solution, lse) << "\n";
+    // double * solution = parallel_system_solver(lse, number_of_threads);
+    double * solution = sequential_system_solver(lse, "debug.txt");
 }
 
 int main(){
+
+    typedef std::chrono::high_resolution_clock Time;
+    typedef std::chrono::milliseconds ms;
+    typedef std::chrono::duration<float> fsec;
     
+    auto start = Time::now();
     solve_system();
+    auto end = Time::now();
+
+    fsec perf_time = end - start;
+    cout << perf_time.count() << "\n";
 
     return 0;
 }
--- parallel_equation_solver.cpp
+++ parallel_equation_solver.cpp
@@ -9,24 +9,32 @@ void manager_thread(int thread_index, linear_system_of_equations lse, double * s
     int number_sums = lse.unknowns_no / number_of_threads;
     if(thread_index < lse.unknowns_no % number_of_threads) number_sums += 1;
 
-    bool * has_been_checked = new bool[lse.unknowns_no];
-    memset(has_been_checked, false, lse.unknowns_no);
+    // bool * has_been_checked = new bool[lse.unknowns_no];
+    // for(int i = 0; i < lse.unknowns_no; i++)has_been_checked[i] = false;
 
     int managed_sums_count = 0;
-    do {
-        for(int sum_index = lse.unknowns_no - 1; sum_index > -1; sum_index--){
-            if(!has_been_checked[sum_index] && needed_counter[sum_index] + sum_index == lse.unknowns_no - 1){
-                for(int local_sum_index = thread_index; local_sum_index < lse.unknowns_no; local_sum_index += number_of_threads){
-                    if(sum_index > local_sum_index){
-                        solution[local_sum_index] -= solution[sum_index] * lse.coefficients[local_sum_index][sum_index] / lse.coefficients[local_sum_index][local_sum_index];
-                        needed_counter[local_sum_index] += 1;
-                        if(needed_counter[local_sum_index] + local_sum_index == lse.unknowns_no - 1) managed_sums_count += 1;
-                    }
-                }
-                has_been_checked[sum_index] = true;
-            }
-        }
-    } while(managed_sums_count != number_sums);
+    // do {
+    //     for(int sum_index = lse.unknowns_no - 1; sum_index > -1; sum_index--){
+    //         if(!has_been_checked[sum_index] && needed_counter[sum_index] + sum_index == lse.unknowns_no - 1){
+    //             for(int local_sum_index = thread_index; local_sum_index < lse.unknowns_no; local_sum_index += number_of_threads){
+    //                 if(sum_index > local_sum_index){
+    //                     solution[local_sum_index] -= solution[sum_index] * lse.coefficients[local_sum_index][sum_index] / lse.coefficients[local_sum_index][local_sum_index];
+    //                     needed_counter[local_sum_index] += 1;
+    //                     if(needed_counter[local_sum_index] + local_sum_index == lse.unknowns_no - 1){
+    //                         managed_sums_count += 1;
+    //                         std::printf("sum %d completed\n", local_sum_index);
+    //                     }
+    //                 }
+    //             }
+    //             has_been_checked[sum_index] = true;
+    //         }
+    //     }
+    // } while(managed_sums_count != number_sums);
+
+    for(int i = 0; i < lse.unknowns_no; i++){
+        std::printf("thread %d, needed_counter[%d] = %d\n", thread_index, i, needed_counter[i]);
+    }
+
 }
 
 double * parallel_system_solver(linear_system_of_equations lse, int number_of_threads){
@@ -40,9 +48,9 @@ double * parallel_system_solver(linear_system_of_equations lse, int number_of_th
     is_finished[lse.unknowns_no - 1] = true;
     needed_counter[lse.unknowns_no - 1] = 0;
     std::vector<std::thread> threads; 
-    for(int i = lse.unknowns_no - 1; i > -1; i--){
-        threads.push_back(std::thread(manager_thread, std::ref(solution), number_of_threads, std::ref(needed_counter)));
-        threads.back().join();
+    for(int i = lse.unknowns_no - 2; i > -1; i--){
+        auto th = std::thread(&manager_thread, i, lse, std::ref(solution), number_of_threads, std::ref(needed_counter));
+        th.join();
     }
 
     return solution;
